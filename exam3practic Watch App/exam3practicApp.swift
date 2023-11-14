f2a14d34bd10890ca6add7601af3b50b44021772
bwaobikeze <bwaobikeze@my.lonestar.edu>
2023-11-14 07:20:47 -0600

    Initial Commit

---
+++ exam3practic Watch App/exam3practicApp.swift
@@ -0,0 +1,17 @@
+//
+//  exam3practicApp.swift
+//  exam3practic Watch App
+//
+//  Created by brian waobikeze on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct exam3practic_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ exam3practic/exam3practicApp.swift
@@ -0,0 +1,17 @@
+//
+//  exam3practicApp.swift
+//  exam3practic
+//
+//  Created by brian waobikeze on 11/14/23.
+//
+
+import SwiftUI
+
+@main
+struct exam3practicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
